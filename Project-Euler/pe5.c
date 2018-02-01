6802b04fd5902c17399e7e5a39aa061571f9900a
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-02-01 19:36:18 +0900

    new questions

--- Project-Euler/pe5.c
+++ Project-Euler/pe5.c
@@ -6,24 +6,29 @@ clock_t start;
 
 //1 から 20 までの整数すべてで割り切れる最小の整数は何か？
 
-int pe5(void){
-  int count = 0;
-  for (int ans = 1;;ans++) {
-    count = 0;
-    for (int i = 1; i <= 20; i++) {
-      if (ans % i == 0) {
-        count++;
-        if (count == 20) {
-          return ans;
-        }
-      }
+int is_prime(int n) {
+  for (int i=2; i<n; i++) {
+    if (n % i == 0){
+      return 0;
     }
   }
+  return 1;
+}
+
+void pe5(void){
+  int ans = 1;
+  for (int i = 2; i <= 20; i++) {
+    if(is_prime(i)){
+      ans *= i;
+      printf("%d\n", i);
+    }
+  }
+  printf("ans : %d\n", ans);
 }
 
 int main(void){
   start = clock();
-  printf("%d\n", pe5());
+  pe5();
   printf("time : %ld[ms]\n", clock() - start);
   return 0;
 }
